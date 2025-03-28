9eedd45595c63a8b78dfe23702dc7e10ed9aed80
Abraham2106 <165726674+Abraham2106@users.noreply.github.com>
2025-03-27 23:53:39 -0600

    `LeetCode\Medium\2456`

---
+++ PythonSolutions/Medium/2456.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def mostPopularCreator(self, creators, ids, views):
+
+        cDict = {key: 0  for key in creators}  
+        sDict = {key: ''  for key in creators} 
+        vDict = {key: 0 for key in creators}  
+
+        for i in range(len(creators)):
+            creator = creators[i]
+            videoId = ids[i]
+            view = views[i]
+
+            cDict[creator] += view
+
+            if view > vDict[creator] or (view == vDict[creator] and videoId < sDict[creator]):
+                vDict[creator] = view
+                sDict[creator] = videoId
+        
+        mostPopular = max(cDict.values())
+        
+        result = []
+        for creator in cDict:
+            if cDict[creator] == mostPopular:
+                maxId = sDict[creator]
+                if maxId == '':
+                    maxId = ids[creators.index(creator)]
+                
+                result.append([creator, maxId])
+
+        return result
