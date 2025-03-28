dabf98492f78531005a6edcc7a121d3371747895
Abraham2106 <165726674+Abraham2106@users.noreply.github.com>
2025-03-28 12:31:14 -0600

    `PythonSolutions/East/66`

---
+++ PythonSolutions/Easy/65.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def plusOne(self, digits):
+        l = len(digits)
+        
+        for i in range(l-1, -1, -1):
+            if digits[i] + 1 > 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        
+        return [1] + digits
