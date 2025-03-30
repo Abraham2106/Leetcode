f982b1a3a0b3790c2adb7f7f6ceac9debdc5db2d
Abraham2106 <165726674+Abraham2106@users.noreply.github.com>
2025-03-30 12:45:44 -0600

    Fix 763

--- PythonSolutions/Medium/763.py
+++ PythonSolutions/Medium/763.py
@@ -1,6 +1,5 @@
 class Solution(object):
     def partitionLabels(self, s):
-        # Save the last position of each character in the string
         last_index = {}
         i = 0
         while i < len(s):
@@ -10,10 +9,9 @@ class Solution(object):
         max_size = 0
         result = []
         idx = 0  
-    
+
         # Go through the string and find the biggest possible partitions
         while idx < len(s):
-
             actual_char = s[idx]  
             max_size = last_index[actual_char]  
             i = idx
@@ -22,11 +20,9 @@ class Solution(object):
             while i <= max_size:
                 max_size = max(max_size, last_index[s[i]])
                 i += 1
-
             # Add the size of the partition to the result
             result.append(max_size - idx + 1)
-
             # Move to the next partition
             idx = max_size + 1
-
+            
         return result
