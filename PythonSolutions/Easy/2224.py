9aad01a077345322bbc8de6075ef3b4848813170
Abraham2106 <165726674+Abraham2106@users.noreply.github.com>
2025-04-01 17:16:06 -0600

    `PythonSolutions/Easy/2224`

---
+++ PythonSolutions/Easy/2224.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def convertTime(self, current, correct):
+
+        total_current = int(current[:2]) * 60 + int(current[3:])
+        total_correct = int(correct[:2]) * 60 + int(correct[3:])
+
+        total_ans = total_correct - total_current
+
+        ans = 0
+        ans = 0
+
+        for value in [60, 15, 5, 1]:
+
+            ans += total_ans // value
+            total_ans %= value
+
+        return ans
+        """
+        :type current: str
+        :type correct: str
+        :rtype: int
+        """
+        
