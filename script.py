a69bbfe9adceab729f20d9ebe07b85c217b44caa
Abraham2106 <165726674+Abraham2106@users.noreply.github.com>
2025-04-04 21:57:57 -0600

    delete script

--- script.py
+++
@@ -1,9 +0,0 @@
-import os
-
-folder = "C:\\Users\\solan\\OneDrive\\Documents\\Leetcode\\Leetcode\\JavaSolutions\\Easy"  # change this to your actual folder name
-for filename in os.listdir(folder):
-    if filename.endswith(".py"):
-        number = filename.split(".")[0]
-        new_name = number.zfill(4) + ".py"  # pad to 4 digits
-        os.rename(os.path.join(folder, filename), os.path.join(folder, new_name))
-        
