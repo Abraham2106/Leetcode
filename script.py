6cda15915338d1ce30095ed930bbdcfdb18e649d
Abraham2106 <165726674+Abraham2106@users.noreply.github.com>
2025-04-04 21:57:07 -0600

    .ignore

--- script.py
+++ script.py
@@ -6,3 +6,4 @@ for filename in os.listdir(folder):
         number = filename.split(".")[0]
         new_name = number.zfill(4) + ".py"  # pad to 4 digits
         os.rename(os.path.join(folder, filename), os.path.join(folder, new_name))
+        
